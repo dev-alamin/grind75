d550b3a3f6197c8b5fe35efaf04394465ba98e1f
Al Amin <hmalaminmb4@gmail.com>
2025-04-15 17:24:15 +0600

    Merge two sorted lists - link list

---
+++ day-2_merge_two_sorted_list.php
@@ -0,0 +1,36 @@
+<?php
+class ListNode {
+    public $val = 0;
+    public $next = null;
+
+    public function __construct( $val = 0, $next = null ) {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+
+function mergeTwoLists( $list1, $list2 ) {
+    $dummy = new ListNode();
+    $current = $dummy;
+
+    while( $list1 != null && $list2 != null ) {
+        if( $list1->val < $list2->val ) {
+            $current->next = $list1;
+            $list1 = $list1->next;
+        }else{
+            $current->next = $list2;
+            $list2 = $list2->next;
+        }
+
+        $current = $current->next;
+    }
+
+    // Add remaining
+    if( $list1 != null ) {
+        $current->next = $list1;
+    }else{
+        $current->next = $list2;
+    }
+
+    return $dummy->next;
+}
